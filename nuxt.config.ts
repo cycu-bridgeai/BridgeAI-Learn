d5b9114128464ec506f9139239702f3a42ed0d42
Jia-Yin <jywglady@gmail.com>
2026-03-26 11:00:14 +0800

    fix: deploy action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-01-01',
   app: {
-    baseURL: process.env.GITHUB_PAGES ? '/BridgeAI-Learn/' : '/',
+    baseURL: '/BridgeAI-Learn/',
   },
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   content: {
